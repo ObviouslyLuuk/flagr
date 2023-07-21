f85a961fdb294d1bc751c1ded4f38d801caf7c7d
ObviouslyLuuk <54840183+ObviouslyLuuk@users.noreply.github.com>
2023-07-21 15:17:43 +0200

    fixed the counting system of outcomes (now not counting each wrong guess within a single question); removed end screen and opted for the "next" button appearing on the correct flag; now always showing score;

--- flag_game.js
+++ flag_game.js
@@ -5,6 +5,7 @@ IMG_EXT = ".svg"
 
 FLAG_COUNT = 9
 var correct_code = null
+var already_guessed = false
 
 var code2country = null
 var gdp_probs = null
@@ -153,19 +154,28 @@ function init_flag_game() {
     if (code2country == null || flag_color_dist == null || flag_mse == null || flag_mse_flips == null || flag_mse_rotations == null || flag_edges_mse == null || gdp_probs == null)
         return
 
-    let question_div = create_and_append("h1", document.body, "question_div")
+    let top_div = create_and_append("div", document.body, "top_div")
+    top_div.style.display = "grid"
+    top_div.style["align-items"] = "center"
+    top_div.style["justify-items"] = "center"
+    top_div.style.gridTemplateColumns = "1fr 1fr 1fr"
+    top_div.style.width = "100%"
 
-    // let idx = Math.floor(Math.random() * country_codes.length)
-    // correct_code = country_codes[idx]
+    // Display SCORE and score percentage
+    let score_div = create_and_append("h2", top_div, "score_div")
+    update_score()
+    let question_div = create_and_append("h1", top_div, "question_div")
+    let timer_div = create_and_append("h2", top_div, "timer_div")
+
+    // Select country
+    already_guessed = false
     correct_code = get_new_flag()
     let idx = country_codes.indexOf(correct_code)
     add_to_recent_flags(correct_code)
     let country = code2country[correct_code]
     question_div.innerHTML = country
 
-    // Select closest n flags based on flag_color_dist, flag_mse and flag_edges_mse, weighted equally
-    // where n = FLAG_COUNT - 1
-    // let closest_flags = find_closest_flags(idx)
+    // Get closest flags
     let [closest_indices, distances] = find_weighted_closest_n(
         [flag_color_dist, flag_mse, flag_mse_flips, flag_mse_rotations, flag_edges_mse], idx, FLAG_COUNT - 1, DEFAULT_WEIGHTS)
     let closest_flags = closest_indices.map(i => country_codes[i])
@@ -175,10 +185,12 @@ function init_flag_game() {
     // For each image in the grid set the src to the flag of the country
     let imgs = div.getElementsByTagName("img")
     imgs[0].src = DATA_DIR + correct_code + IMG_EXT
+    imgs[0].dataset.code = correct_code
+    imgs[0].dataset.dist = 0
     for (let i = 1; i < imgs.length; i++) {
         let img = imgs[i]
-        // let country_code = country_codes[Math.floor(Math.random() * country_codes.length)]
         let country_code = closest_flags[i-1]
+        img.dataset.code = country_code
         img.dataset.dist = distances[i-1]
 
         // Set src to the flag of the country
@@ -192,24 +204,29 @@ function init_flag_game() {
             // If already guessed, do nothing
             if (img.dataset.guessed == "true")
                 return
-            // If endgame overlay is shown, do nothing
-            if (document.getElementById("endgame_overlay") != null)
-                return
+
+            let outcome = check_answer(img)
 
             img.dataset.guessed = "true"
+            already_guessed = true
 
-            // Overlay country name on image
-            overlay_country_name(img)
+            if (outcome == true) {
+                // Add button to go to next question
+                overlay_next_button(img)
 
-            // Add animation that makes the image smaller and then bigger again
-            img.style.animation = "pop 0.5s"
-            img.style.animationFillMode = "forwards"
-            // Remove animation after 0.5s
-            setTimeout(function() {
-                img.style.animation = ""
-            }, 500)
-            
-            check_answer(img)
+                // Overlay country name on each image that wasn't guessed
+                let imgs = document.getElementById("flag_grid").getElementsByTagName("img")
+                for (let im of imgs) {
+                    if (im.dataset.guessed == "true") continue
+
+                    im.dataset.guessed = "true"
+                    overlay_country_name(im)
+                }
+            } else {
+                overlay_country_name(img)
+            }
+            add_pop_animation(img)
+            update_score()
         }
         // Set cursor to pointer
         img.style.cursor = "pointer"
@@ -230,6 +247,16 @@ function init_flag_game() {
     shuffle_elements(div)
 }
 
+/** Add animation that makes the image smaller and then bigger again */
+function add_pop_animation(elem) {
+    elem.style.animation = "pop 0.5s"
+    elem.style.animationFillMode = "forwards"
+    // Remove animation after 0.5s
+    setTimeout(function() {
+        elem.style.animation = ""
+    }, 500)
+}
+
 function add_to_recent_flags(code) {
     recent_flags.unshift(code)
     if (recent_flags.length > 20)
@@ -243,15 +270,16 @@ function add_to_recent_outcomes(outcome) {
 
 /** Function for getting new "question" flag based on different probabilities */
 function get_new_flag() {
+    let success_weight = mean(recent_outcomes.slice(0, 10))
+    // If no outcomes yet, set to 0
+    if (recent_outcomes.length < 10)
+        success_weight = 0
+    console.log("\nsuccess weight: ", success_weight)
+
     // Assign probabilities to each flag
     let probabilities = []
     for (let i = 0; i < country_codes.length; i++) {
         let code = country_codes[i]
-
-        let success_weight = mean(recent_outcomes.slice(0, 10))
-        // If no outcomes yet, set to 0
-        if (recent_outcomes.length < 10)
-            success_weight = 0
 
         let weighted_gdp_prob = success_weight * (1-gdp_probs[code]) + (1 - success_weight) * gdp_probs[code]
         let prob = get_recency_probability(code) * weighted_gdp_prob
@@ -260,11 +288,19 @@ function get_new_flag() {
     }
     // Normalise probabilities
     probabilities = normalise(probabilities)
+
     // Sample from the distribution
-    let code = multinomial_sample(country_codes, probabilities)
+    // let code = multinomial_sample(country_codes, probabilities)
+    // Greedy
+    let code = country_codes[probabilities.indexOf(Math.max(...probabilities))]
+
+    // Print info
     console.log(code2country[code])
     console.log("GDP probability: ", gdp_probs[code])
-    console.log("Final probability rank: ", probabilities.indexOf(probabilities.find(p => p == probabilities[code])))
+    let final_prob = probabilities[country_codes.indexOf(code)]
+    probabilities.sort((a, b) => b - a)
+    console.log("Final probability rank: ", probabilities.indexOf(probabilities.find(p => p == final_prob)))
+
     return code
 }
 
@@ -287,62 +323,36 @@ function overlay_country_name(img) {
     overlay.innerHTML = code2country[img.src.split("/").pop().split(".")[0]]
 }
 
-function display_endgame_overlay(win, message) {
-    let overlay = create_and_append("div", document.body, "endgame_overlay", "overlay")
-    overlay.style.display = "block"
-    overlay.style.position = "fixed"
+function overlay_next_button(img) {
+    img.style.cursor = "default"
+    img.style.filter = "brightness(0.5)"
+    let overlay = create_and_append("div", img.parentElement, null, "country_name_overlay")
+    overlay.innerHTML = "Correct!"
+    overlay.style.display = "grid"
 
-    let endgame_div = create_and_append("div", overlay, "endgame_div", "overlay-content")
-    // Add message as h1
-    let h1 = create_and_append("h1", endgame_div)
-    h1.innerHTML = message
-
-    // Display SCORE and score percentage
-    let score_div = create_and_append("h2", endgame_div, "score_div")
-    score_div.innerHTML = "Score: " + sum(recent_outcomes) + " / " + recent_outcomes.length + " (" + Math.round(mean(recent_outcomes) * 100) + "%)"
-    score_div.style.marginBottom = "20px"
-
-    // Buttons div so they go next to each other
-    let buttons_div = create_and_append("div", endgame_div, "buttons_div")
-    buttons_div.style.display = "flex"
-
-    // Add restart button
-    let restart_button = create_and_append("button", buttons_div, "restart_button", "btn btn-secondary")
+    let restart_button = create_and_append("button", overlay, "restart_button", "btn btn-secondary")
     restart_button.innerHTML = "Next"
     restart_button.onclick = function() {
         document.getElementById("flag_grid").remove()
-        document.getElementById("question_div").remove()
-        document.getElementById("endgame_overlay").remove()
+        document.getElementById("top_div").remove()
         init_flag_game()
     }
-    // Only add try again button if lost
-    if (!win) {
-        // Try again button
-        let try_again_button = create_and_append("button", buttons_div, "try_again_button", "btn btn-secondary")
-        try_again_button.innerHTML = "Try again"
-        try_again_button.onclick = function() {
-            document.getElementById("endgame_overlay").remove()
-        }
+}
+
+function update_score() {
+    score_div.innerHTML = "Score: " + sum(recent_outcomes) + " / " + recent_outcomes.length
+
+    if (recent_outcomes.length > 0) {
+        score_div.innerHTML += " (" + Math.round(mean(recent_outcomes) * 100) + "%)"
     }
 }
 
 function check_answer(img) {
-    if (img.src.endsWith(correct_code + IMG_EXT)) {
-        // Correct
-        add_to_recent_outcomes(1)
-        display_endgame_overlay(true, "Correct!")
-
-        // Overlay country name on each image
-        let imgs = document.getElementById("flag_grid").getElementsByTagName("img")
-        for (let img of imgs) {
-            img.dataset.guessed = "true"
-            overlay_country_name(img)
-        }
-    } else {
-        // Incorrect
-        add_to_recent_outcomes(0)
-        display_endgame_overlay(false, "Incorrect!")
+    let outcome = (img.dataset.code == correct_code)
+    if (!already_guessed) {
+        add_to_recent_outcomes(outcome)
     }
+    return outcome
 }
 
 function find_weighted_closest_n(matrices, idx, n, weights=[1, 1, 1]) {
@@ -391,37 +401,6 @@ function find_weighted_closest_n(matrices, idx, n, weights=[1, 1, 1]) {
     }
 
     return [closest, closest_dist]
-}
-
-function find_closest_flags(idx) {
-    let closest_flags = []
-
-    // Find closest flag based on flag_color_dist
-    closest_flags.push(country_codes[find_closest(flag_color_dist, idx)])
-
-    // Find closest flag based on flag_mse
-    closest_flags.push(country_codes[find_closest(flag_mse, idx)])
-
-    // Find closest flag based on flag_edges_mse
-    closest_flags.push(country_codes[find_closest(flag_edges_mse, idx)])
-
-    return closest_flags
-}
-
-function find_closest(matrix, idx) {
-    let closest = null
-    let closest_dist = Infinity
-    for (let i = 0; i < matrix.length; i++) {
-        if (i == idx)
-            continue
-
-        let dist = matrix[idx][i]
-        if (dist < closest_dist) {
-            closest_dist = dist
-            closest = i
-        }
-    }
-    return closest
 }
 
 /** Add div with 3x3 image grid to the parent element */
