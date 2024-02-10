4d7a620b47f24cbb126f88ea5e5bab0a5c0fc563
ObviouslyLuuk <54840183+ObviouslyLuuk@users.noreply.github.com>
2024-02-10 19:58:42 +0100

    removed updating stats for every guess because it was slow and also caused an error; added flag_imagine.js for not showing grid to choose from but just hiding and showing the correct flag;

--- flag_imagine.js
+++ flag_imagine.js
@@ -1,9 +1,6 @@
 ///////////////////////////////////////////////////////////////////////////////
-// FLAGR
-// A game to test your knowledge of flags
-// By: ObviouslyLuuk
-//
-// This file contains the main game functions and variables
+// This file contains an adaptation of the game where there are no
+// options, just a "show" button that shows the correct answer.
 ///////////////////////////////////////////////////////////////////////////////
 
 
@@ -21,42 +18,7 @@ function get_flag_src(code) {
     return DATA_DIR + code + IMG_EXT
 }
 
-grid_configs = {
-    1: "1x1",
-    2: "1x2",
-    3: "1x3",
-    4: "2x2",
-    5: "2x3",
-    6: "2x3",
-    7: "2x4",
-    8: "2x4",
-    9: "3x3",
-    16: "4x4",
-}
-
-METRICS = ["color", "mse", "mse_flips", "mse_rotations", "edges_mse"]
-METRIC_NAMES = ["Color Palette", "Exact", "Flips", "Rotations", "Edges"]
-
 // Global variables
-var flag_color_dist = null
-var flag_mse = null
-var flag_mse_flips = null
-var flag_mse_rotations = null
-var flag_edges_mse = null
-
-WEIGHT_PRESETS = {
-    "balanced": [100, 8, 34, 0, 13], // Seems to be well balanced
-    "color": [100, 2, 2, 0, 10], // Emphasis on color (nice for aesthetics)
-}
-DEFAULT_WEIGHTS = WEIGHT_PRESETS["balanced"]
-
-EXAMPLE_COUNTRIES = ["Greece", "Bahamas", "Ireland", "Japan", "Denmark"] // Nice examples to show the different metrics
-
-DEFAULT_GAME_MODE = "endless" // "endless" or "all_flags" or "fifteen" or "training"
-FLAG_COUNT = 9
-var correct_code = null
-var already_guessed = false
-
 var code2country = null
 var gdp_probs = null
 var country_codes = []
@@ -66,12 +28,12 @@ var countries = []
 var recent_flags = []
 var recently_correct = []
 var recent_outcomes = []
-var EMA = 0 // Exponential moving average of recent_outcomes
+var EMA = 0.5 // Exponential moving average of recent_outcomes
 EMA_ALPHA = 0.3 // Smoothing factor for EMA
 
 // Game mode variables
 var max_flags, timed, get_flag_fn, start_time, timer
-var game_mode = DEFAULT_GAME_MODE
+var game_mode = null
 
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -149,46 +111,6 @@ fetch('json/GDP_probabilities.json')
         gdp_probs = data
         init_flag_game(true)
     })
-fetch('json/color_dist_matrix.json')
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        flag_color_dist = data
-        init_flag_game(true)
-    })
-fetch('json/flag_mse_matrix.json')
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        flag_mse = data
-        init_flag_game(true)
-    })
-fetch('json/flag_mse_matrix_flips.json')
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        flag_mse_flips = data
-        init_flag_game(true)
-    })
-fetch('json/flag_mse_matrix_rotations.json')
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        flag_mse_rotations = data
-        init_flag_game(true)
-    })
-fetch('json/flag_edges_mse_matrix.json')
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        flag_edges_mse = data
-        init_flag_game(true)
-    })
 
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -224,7 +146,7 @@ function save_stats(stats) {
 
 function init_flag_game(reset=false, flag=null) {
     // Wait for all data to be loaded
-    if (code2country == null || flag_color_dist == null || flag_mse == null || flag_mse_flips == null || flag_mse_rotations == null || flag_edges_mse == null || gdp_probs == null)
+    if (code2country == null || gdp_probs == null)
         return
 
     // Remove previous flag grid and top div
@@ -251,23 +173,10 @@ function init_flag_game(reset=false, flag=null) {
 
     // Gamemode config
     switch (game_mode) {
-        case "endless":
+        default:
             max_flags = Infinity
             timed = false
             get_flag_fn = get_new_flag 
-            break;
-        case "all_flags":
-            max_flags = country_codes.length
-            timed = true
-            get_flag_fn = get_new_flag_no_repeat
-            break;
-        case "fifteen":
-            max_flags = 15
-            timed = true
-            break;
-        case "training":
-            max_flags = 15
-            timed = false
             break;
     }
 
@@ -292,12 +201,7 @@ function init_flag_game(reset=false, flag=null) {
     let country = code2country[correct_code]
     question_div.innerHTML = country
 
-    // Get closest flags
-    let [closest_indices, distances] = find_weighted_closest_n(
-        [flag_color_dist, flag_mse, flag_mse_flips, flag_mse_rotations, flag_edges_mse], idx, FLAG_COUNT - 1, get_weights())
-    let closest_flags = closest_indices.map(i => country_codes[i])
-
-    let div = add_image_grid(document.body, FLAG_COUNT, id="flag_grid")
+    let div = add_image_grid(document.body, id="flag_grid")
 
     // For each image in the grid set the src to the flag of the country
     let imgs = div.getElementsByTagName("img")
@@ -314,20 +218,20 @@ function init_flag_game(reset=false, flag=null) {
         img.src = DATA_DIR + country_code + IMG_EXT
     }
 
-    // Add function onclick to each image to check if it's the correct one
+    // Add function onclick to each image to show the flag
     for (let i = 0; i < imgs.length; i++) {
         let img = imgs[i]
         img.onclick = function() {
-            // If already guessed, do nothing
-            if (img.dataset.guessed == "true")
-                return
+            let outcome = true
+            img.style.filter = "brightness(1)"
 
-            let outcome = check_answer(img)
+            // Make country_name_overlay fade out with animation "fade_out"
+            let overlay = img.parentElement.getElementsByClassName("country_name_overlay")[0]
+            overlay.style.animation = "fade_out 0.5s"
+            overlay.style.animationFillMode = "forwards"
+
             update_score()
             if (check_if_over()) return
-
-            img.dataset.guessed = "true"
-            already_guessed = true
 
             if (outcome == true) {
                 recently_correct.unshift(img.dataset.code)
@@ -336,34 +240,17 @@ function init_flag_game(reset=false, flag=null) {
                     init_flag_game()
                 }
                 else {
-                    // Add button to go to next question
-                    overlay_next_button(img)
-
-                    // Overlay country name on each image that wasn't guessed
-                    disable_all_flags()
+                    // Add click event to next flag
+                    img.onclick = function() {
+                        init_flag_game()
+                    }
                 }
-            } else {
-                overlay_country_name(img)
             }
             add_pop_animation(img)
         }
         // Set cursor to pointer
         img.style.cursor = "pointer"
-
-        // Make colour lighter if hovering over image
-        img.onmouseover = function() {
-            if (img.dataset.guessed == "true") return
-
-            img.style.filter = "brightness(0.7)"
-        }
-        img.onmouseout = function() {
-            if (img.dataset.guessed == "true") return
-
-            img.style.filter = "brightness(1)"
-        }
     }
-
-    shuffle_elements(div)
 }
 
 function set_gamemode(gamemode) {
@@ -395,21 +282,6 @@ function update_timer() {
     timer_div.innerHTML = time.toFixed(1) + "s"
 }
 
-/** Function to check if the clicked flag is the correct one */
-function check_answer(img) {
-    let outcome = (img.dataset.code == correct_code)
-    if (!already_guessed) {
-        add_to_recent_outcomes(outcome)
-        EMA = outcome*EMA_ALPHA + EMA*(1-EMA_ALPHA)
-
-        let stats = get_stats()
-        stats["flag_stats"][correct_code]["total"] += 1
-        stats["flag_stats"][correct_code]["correct"] += outcome
-        save_stats(stats)
-    }
-    return outcome
-}
-
 /** Function to check if the game is over */
 function check_if_over() {
     if (recent_outcomes.length >= max_flags) {
@@ -427,103 +299,15 @@ function check_if_over() {
 
 
 ///////////////////////////////////////////////////////////////////////////////
-// FLAG SIMILARITY FUNCTIONS
-///////////////////////////////////////////////////////////////////////////////
-
-function get_weights() {
-    // If not init yet, return default weights
-    if (document.getElementById("similarity_div") == null)
-        return DEFAULT_WEIGHTS
-
-    let weights = []
-    for (let i = 0; i < 5; i++) {
-        let slider = document.getElementById(METRICS[i]+"_slider")
-        weights.push(slider.value)
-    }
-    return weights
-}
-/** Function to find the closest n flags to the given flag */
-function find_weighted_closest_n(matrices, idx, n, weights=[1, 1, 1]) {
-    // Make sure there's as many weights as matrices
-    if (weights.length != matrices.length) {
-        console.error("Weights and matrices must be of same length")
-        return null }
-
-    let closest = []
-    let closest_dist = []
-    for (let i = 0; i < matrices[0].length; i++) {
-        if (i == idx)
-            continue
-
-        // Dist is weighted sum of distances in each matrix
-        let dist = 0
-        for (let j = 0; j < matrices.length; j++) {
-            dist += weights[j] * matrices[j][idx][i]
-        }
-        
-        if (closest.length < n) {
-            closest.push(i)
-            closest_dist.push(dist)
-        } else {
-            let max_dist = Math.max(...closest_dist)
-            let max_idx = closest_dist.indexOf(max_dist)
-            if (dist < max_dist) {
-                closest[max_idx] = i
-                closest_dist[max_idx] = dist
-            }
-        }
-    }
-    // Sort closest by closest_dist
-    for (let i = 0; i < closest.length; i++) {
-        for (let j = i + 1; j < closest.length; j++) {
-            if (closest_dist[i] > closest_dist[j]) {
-                let temp = closest_dist[i]
-                closest_dist[i] = closest_dist[j]
-                closest_dist[j] = temp
-
-                temp = closest[i]
-                closest[i] = closest[j]
-                closest[j] = temp
-            }
-        }
-    }
-
-    return [closest, closest_dist]
-}
-
-/** Function to change preset for the metric weights */
-function set_metric_preset(preset) {
-    if (document.getElementById("similarity_div") == null)
-        init_flag_similarity()
-
-    for (let i in METRICS) {
-        let slider = document.getElementById(METRICS[i]+"_slider")
-        slider.value = WEIGHT_PRESETS[preset][i]
-    }
-    enable_buttons("metrics_buttons_div")
-    // Disable the button that was clicked
-    let button = document.getElementById("metric_"+preset+"_btn")
-    button.classList.add("disabled")
-}
-
-
-///////////////////////////////////////////////////////////////////////////////
 // GET NEW FLAG FUNCTIONS
 ///////////////////////////////////////////////////////////////////////////////
 
-/** Function for getting new "question" flag randomly, no repeats */
-function get_new_flag_no_repeat() {
-    let possible_flags = country_codes.filter(code => !recent_flags.includes(code))
-    let idx = Math.floor(Math.random() * possible_flags.length)
-    return possible_flags[idx]
-}
-
-/** Function for getting new "question" flag based on different probabilities */
+/** Function for getting new "question" flag based on different probabilities. No repeats */
 function get_new_flag() {
     let success_rate = mean(recent_outcomes.slice(0, 10))
-    // If no outcomes yet, set to 0
+    // If no outcomes yet, set to 0.5
     if (recent_outcomes.length < 5)
-        success_rate = 0
+        success_rate = 0.5
     console.log("\nrecent success rate: ", success_rate)
     console.log("EMA: ", EMA)
 
@@ -572,20 +356,10 @@ function get_new_flag() {
 
 /** Probability penalty based on recency */
 function get_recency_probability(code) {
-    let idx = recently_correct.indexOf(code)
-
-    // If not in recently_correct
-    if (idx == -1) {
-        idx = recent_flags.indexOf(code)
-        if (idx == -1)
-            return 1
-
-        let unbounded = (idx/3)**2 // recover after 3 questions
-        return Math.min(unbounded, 1)
-    }
-
-    unbounded = (idx/200)**2 // recover after 200 questions
-    return Math.min(unbounded, 1)
+    // If the flag is in the recent_flags list, return 0
+    if (recent_flags.includes(code))
+        return 0
+    return 1
 }
 
 function success_to_difficulty(success_rate) {
@@ -605,41 +379,26 @@ function add_top_div(parent) {
     top_div.style.gridTemplateColumns = "1fr 1fr 1fr"
     top_div.style.width = "90vw"
     top_div.style["text-align"] = "center"
-    top_div.style["min-height"] = "9em"
+    top_div.style["min-height"] = "2em"
     return top_div
 };
 /** Add div with 3x3 image grid to the parent element */
-function add_image_grid(parent, count, id="flag_grid") {
+function add_image_grid(parent, id="flag_grid") {
     // Create div element
-    let div = create_and_append("div", parent, id, "image_grid image_grid"+grid_configs[FLAG_COUNT])
+    let div = create_and_append("div", parent, id, "image_grid")
+    div.style["min-height"] = "45vh"
 
-    // Create 3x3 grid of images
-    for (let i = 0; i < count; i++) {
-        let img_container = create_and_append("div", div, null, "img_container")
-        create_and_append("img", img_container, null, "flag")
-    }
+    // Create 1x1 image grid
+    let img_container = create_and_append("div", div, null, "img_container")
+    let overlay = create_and_append("div", img_container, null, "country_name_overlay")
+    overlay.style["font-size"] = "5em"
+    overlay.style["font-weight"] = "bold"
+    overlay.innerHTML = "?"
+
+    let img = create_and_append("img", img_container, null, "flag")
+    img.style.filter = "brightness(0)" // Make it black
+    img.style["max-height"] = "40vh"
     return div
-}
-
-function overlay_country_name(img) {
-    img.style.cursor = "default"
-    img.style.filter = "brightness(0.5)"
-    let overlay = create_and_append("div", img.parentElement, null, "country_name_overlay")
-    overlay.innerHTML = code2country[img.src.split("/").pop().split(".")[0]]
-}
-
-function overlay_next_button(img) {
-    img.style.cursor = "default"
-    img.style.filter = "brightness(0.5)"
-    let overlay = create_and_append("div", img.parentElement, null, "country_name_overlay")
-    overlay.innerHTML = "Correct!"
-    overlay.style.display = "grid"
-
-    let restart_button = create_and_append("button", overlay, "restart_button", "btn btn-secondary")
-    restart_button.innerHTML = "Next"
-    restart_button.onclick = function() {
-        init_flag_game()
-    }
 }
 
 function get_score_str() {
@@ -651,18 +410,8 @@ function get_score_str() {
     return str
 }
 function update_score() {
-    score_div.innerHTML = "Score: " + get_score_str()
-}
-
-/** Overlay country name on each image that wasn't guessed and disable clicking */
-function disable_all_flags() {
-    let imgs = document.getElementById("flag_grid").getElementsByTagName("img")
-    for (let im of imgs) {
-        if (im.dataset.guessed == "true") continue
-
-        im.dataset.guessed = "true"
-        overlay_country_name(im)
-    }
+    // score_div.innerHTML = "Score: " + get_score_str()
+    console.log("update score not implemented")
 }
 
 function show_end_screen_overlay() {
